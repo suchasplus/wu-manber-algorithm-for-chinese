4c49c95fe75d0cf499bcd5c86eaf1fe6a874b1db
wengyunhe@gmail.com <wengyunhe@gmail.com@6834fa51-5f4c-000f-8c03-13beb72016b2>
2012-10-18 12:10:18 +0000

    add makefile and README

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "wumanber.h"
 
 using namespace std;
 
@@ -20,7 +21,17 @@ int main(int argc, char* argv[])
     //step3: find patterns using wumanber in paraell
     ifstream text("text");
     while (text>>s)
+    {
+        //interface1
         cout << s << " hit:" << wu.Search(s) << endl;
+        //interface2
+        set<string> res;
+        wu.Search(s, res);
+        for (set<string>::iterator it = res.begin(); it != res.end(); ++it)
+        {
+            cout << "\t" << *it << endl;
+        }
+    }    
     
     return 0;
 }
---
+++ trunk/makefile
@@ -0,0 +1,7 @@
+wumanber_test: wumanber.o
+	g++ -o wumanber_test main.cpp wumanber.o
+wumanber.o: wumanber.h wumanber.cpp
+	g++ -c wumanber.cpp
+clean:
+	rm *.o wumanber_test
+
